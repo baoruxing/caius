bb811a97814bab90249b138b9f806d9bfee914c0
unknown <ruxing.bao@SH-CODE-PC0215.fn.com>
2016-05-20 17:41:54 +0800

    etcd source init

--- etcd/etcdconfigsource.go
+++ etcd/etcdconfigsource.go
@@ -1,9 +1,31 @@
 package etcd
 
+import (
+	"container/list"
+	"github.com/baoruxing/caius/"
+	"strings"
+)
+
 type EtcdWatchedConfigSource struct {
+	valueCache map[string]interface{}
+	connPath   string
+	listeners  List
+}
+
+func (e *EtcdWatchedConfigSource) AddUpdateListener(listener WatchedUpdateListener) {
+	if listener != nil {
+		e.listeners.PushBack(listener)
+	}
 
 }
 
-func (e *EtcdWatchedConfigSource) AddUpdateListener() {
-    ValueCache  map[string] interface{}
+func (e *EtcdWatchedConfigSource) RemoveUpdateListener(listener WatchedUpdateListener) {
+	if listener != nil {
+		e.listeners.Remove(listener)
+	}
+
+}
+
+func (e *EtcdWatchedConfigSource) GetCurrentConfigData() (*map[string]interface{}, error) {
+	return e.valueCache
 }
