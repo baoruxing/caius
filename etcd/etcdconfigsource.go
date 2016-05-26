532f34501049d41080806e80072ea702626a7ac3
unknown <ruxing.bao@SH-CODE-PC0215.fn.com>
2016-05-26 19:03:06 +0800

    Add etcd watcher

--- etcd/etcdconfigsource.go
+++ etcd/etcdconfigsource.go
@@ -3,13 +3,16 @@ package etcd
 import (
 	"container/list"
 	"github.com/baoruxing/caius/"
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
 	"strings"
 )
 
 type EtcdWatchedConfigSource struct {
+	configPath string
 	valueCache map[string]interface{}
-	connPath   string
 	listeners  List
+	KeysAPI    client.KeysAPI
 }
 
 func (e *EtcdWatchedConfigSource) AddUpdateListener(listener WatchedUpdateListener) {
@@ -30,8 +33,47 @@ func (e *EtcdWatchedConfigSource) GetCurrentConfigData() (*map[string]interface{
 	return e.valueCache
 }
 
-func (e *EtcdWatchedConfigSource)
+func NewEtcdWatchedConfigSource(endpoints []string, configPath string) *EtcdWatchedConfigSource {
+	cfg := client.Config{
+		Endpoints:               endpoints,
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	etcdClient, err := client.New(cfg)
+	if err != nil {
+		log.Fatal("Error: cannot connec to etcd:", err)
+	}
+	configSource := &EtcdWatchedConfigSource{
+		configPath: configPath,
+		valueCache: make(map[string]interface{}),
+		listeners:  list.New(),
+		KeysAPI:    client.NewKeysAPI(etcdClient),
+	}
+	go configSource.updateHandler()
+	return configSource
+}
 
-func New() *EtcdWatchedConfigSource {
-    return new(EtcdWatchedConfigSource)
+func (e *EtcdWatchedConfigSource) updateHandler() {
+	api := e.KeysAPI
+	watcher := api.Watcher("configPath", &client.WatcherOptions{
+		Recursive: true,
+	})
+	for {
+		res, err := watcher.Next(context.Background)
+		if err != nil {
+			log.Println("Error watch configPath:", err)
+			break
+		}
+		etcdKey := res.Node.Key
+		splitedKey := trings.Split(etcdKey, "/")
+		sourceKey := splitedKey[len(splitedKey)-1]
+		value := res.Node.Value
+		if res.Action == "expire" {
+
+		} else if res.Action == "create" || res.Action == "set" || res.Action == "update" {
+			e.valueCache[sourceKey] = value
+		} else if res.Action == "delete" {
+			delete(e.valueCache, value)
+		}
+	}
 }
